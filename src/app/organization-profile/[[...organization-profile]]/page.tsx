59b559b65f2c5b05950f3b2f79234ef8f951d075
Temirlan <153745312+temirlannz@users.noreply.github.com>
2024-01-17 17:33:26 +0600

    org-profile style changes

--- src/app/organization-profile/[[...organization-profile]]/page.tsx
+++ src/app/organization-profile/[[...organization-profile]]/page.tsx
@@ -19,12 +19,16 @@ const OrganizationProfilePage = () => {
                 routing="path"
                 appearance={{
                     elements: {
-                        card: 'rounded-md bg-[#F9F9F9] border shadow-none border-gray-200',
+                        rootBox: 'w-full',
+                        card: 'rounded-md bg-[#F9F9F9] border shadow-none border-gray-200 w-full max-w-full',
                         navbar: 'border-r-0',
                         headerTitle: 'text-xl',
                         headerSubtitle: 'text-sm text-muted-foreground',
                         navbarButton: 'font-light',
-                        membersPageInviteButton: 'disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-9 px-4 py-2'
+                        membersPageInviteButton: 'disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-9 px-4 py-2',
+                        menuList__member: 'bg-[#F9F9F9] shadow-md h-[40px] p-0 rounded-md',
+                        menuItem__member: 'rounded-md h-full',
+                        selectOptionsContainer__role: 'bg-[#F9F9F9] shadow-md'
                     }
                 }}
             />
