41f4a49c59151dde48a02157c7eb717be553b9a5
Temirlan <153745312+temirlannz@users.noreply.github.com>
2024-01-18 17:18:34 +0600

    display group name on group page

--- src/app/home/[classId]/[groupId]/loading.tsx
+++ src/app/home/[classId]/[groupId]/loading.tsx
@@ -6,7 +6,7 @@ const Loading = () => {
     return (
         <section className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
             <div className='flex justify-between items-center mt-5 mb-5'>
-                <h1 className='text-xl font-medium'>Students</h1>
+                <Skeleton className='w-[100px] h-[36px]' />
 
                 <div>
                     <Skeleton className='w-[120px] h-[36px]' />
--- src/app/home/[classId]/[groupId]/page.tsx
+++ src/app/home/[classId]/[groupId]/page.tsx
@@ -8,22 +8,26 @@ import Loading from "@/app/home/[classId]/[groupId]/loading";
 const xata = getXataClient();
 
 const getGroup = async (groupId: string) => {
-    return await xata.db.student.filter({ 'group.id': groupId }).getMany();
+    const group = await xata.db.student.filter({ group: groupId }).getMany();
+    const groupName = await xata.db.group.filter({ id: groupId }).select(['name']).getFirst();
+    return { group, groupName };
 }
 
 const Group = async ({ params }: { params: {groupId: string, classId: string} }) => {
-    const group = await getGroup(params.groupId);
+    const groupData = await getGroup(params.groupId);
 
     return (
         <section className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
             <div className='flex justify-between items-center mt-5 mb-5'>
-                <h1 className='text-xl font-medium'>Students</h1>
+                <h1 className='text-xl font-medium'>
+                    { groupData?.groupName?.name }
+                </h1>
 
                 <NewStudent />
             </div>
             
             <Suspense fallback={<Loading />}>
-                <DataTable columns={columns} data={JSON.parse(JSON.stringify(group))} />
+                <DataTable columns={columns} data={JSON.parse(JSON.stringify(groupData.group))} />
             </Suspense>
         </section>
     )
