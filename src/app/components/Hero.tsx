d08d3069a9694a335dad261552135f4d6541e30f
Temirlan <153745312+temirlannz@users.noreply.github.com>
2024-02-13 14:58:16 +0600

    hero blue bg

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,7 +3,7 @@ import {WavyBackground} from "@/components/ui/wavy-background";
 
 const Hero = () => {
     return (
-        <WavyBackground backgroundFill='white' colors={['red', 'blue', 'yellow  ']}>
+        <WavyBackground backgroundFill='white' colors={['red', 'blue', 'yellow']} blur={15}>
             <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-20 text-center lg:pt-32 pb-24 h-[calc(100vh-76px)] sm:h-[calc(100vh-116px)] flex flex-col sm:items-center justify-center'>
                 <h1 className='sm:mx-auto max-w-4xl font-display text-5xl tracking-tight sm:text-8xl font-extrabold text-left sm:text-center'>
                     ATTENDANCE <br/> CHECKER
